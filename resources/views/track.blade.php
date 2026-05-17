179f6af7bc6832d67076864f31938d0475931e1a
bawanabbas20-wq <bawanabbas20@gmail.com>
2026-05-17 21:16:11 +0300

    fix: attempt to read property on null when accessing base track page

--- resources/views/track.blade.php
+++ resources/views/track.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Track Application - {{ $application->tracking_code }}</title>
+    <title>Track Application{{ $application ? ' - ' . $application->tracking_code : '' }}</title>
 
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
